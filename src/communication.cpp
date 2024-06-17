0b3929108f2f3a56dd5bab3090bdbd212b2c76a9
Filip <filwes@student.chalmers.se>
2024-06-17 10:09:47 +0200

    Editing particle addition/removal/reordering

--- src/communication.cpp
+++ src/communication.cpp
@@ -29,8 +29,8 @@ class Communication{
     // Forward communication entails the migration of ghost atoms
     // Backward communication entails the reception of force calculation results
     // Keep an array of arrays of pointers to communication data stored in vectors of IDPOSITION structs
-    std::vector<std::array<std::vector<int3double> , 26>> forward_communication_data;       // Data structure for forward communication : no_subdomains x 26
-    std::vector<std::array<std::vector<int3double> , 26>> backward_communication_data;
+    std::vector<std::array<std::vector<int3double> , 6>> forward_communication_data;       // Data structure for forward communication : no_subdomains x 26
+    std::vector<std::array<std::vector<int3double> , 6>> backward_communication_data;
 
 public:
     Communication(int no_subdomains) : no_subdomains(no_subdomains) {
@@ -39,7 +39,7 @@ public:
         backward_communication_data.resize(no_subdomains);
 
         for (int i = 0; i < no_subdomains; i++) {
-            for (int j = 0; j < 26; j++) {
+            for (int j = 0; j < 6; j++) {
                 forward_communication_data[i][j] = std::vector<int3double>(default_size);
                 backward_communication_data[i][j] = std::vector<int3double>(default_size);
 
@@ -48,39 +48,28 @@ public:
     }
     ~Communication() {}
 
-    // Functionsvoid 
-    void set_forward_communication_data(int subdomain_id, int cell_id, const std::vector<int3double>& data) {
-        if (subdomain_id >= 0 && subdomain_id < no_subdomains && cell_id >= 0 && cell_id < 26) {
-            forward_communication_data[subdomain_id][cell_id] = data;
-        } else {
-            std::cerr << "Error: Index out of bounds in set_forward_communication_data.\n";
-        }
+    // Functions
+    void set_forward_communication_data(int subdomain_id, int neighbouring_subdomain_id, const std::vector<int3double>& data) {
+        forward_communication_data[subdomain_id][neighbouring_subdomain_id] = data;
     }
 
-    void set_backward_communication_data(int subdomain_id, int cell_id, const std::vector<int3double>& data) {
-        if (subdomain_id >= 0 && subdomain_id < no_subdomains && cell_id >= 0 && cell_id < 26) {
-            backward_communication_data[subdomain_id][cell_id] = data;
-        } else {
-            std::cerr << "Error: Index out of bounds in set_backward_communication_data.\n";
-        }
+    void set_backward_communication_data(int subdomain_id, int neighbouring_subdomain_id, const std::vector<int3double>& data) {
+        backward_communication_data[subdomain_id][neighbouring_subdomain_id] = data;
     }
 
     std::vector<int3double> get_forward_communication_data(int subdomain_id, int neighbouring_subdomain_id) const {
-        if (subdomain_id >= 0 && subdomain_id < no_subdomains && neighbouring_subdomain_id >= 0 && neighbouring_subdomain_id < 26) {
-            return forward_communication_data[subdomain_id][neighbouring_subdomain_id];
-        } else {
-            std::cerr << "Error: Index out of bounds in get_forward_communication_data.\n";
-            return std::vector<int3double>();
-        }
+        return forward_communication_data[subdomain_id][neighbouring_subdomain_id];
     }
 
     std::vector<int3double> get_backward_communication_data(int subdomain_id, int neighbouring_subdomain_id) const {
-        if (subdomain_id >= 0 && subdomain_id < no_subdomains && neighbouring_subdomain_id >= 0 && neighbouring_subdomain_id < 26) {
-            return backward_communication_data[subdomain_id][neighbouring_subdomain_id];
-        } else {
-            std::cerr << "Error: Index out of bounds in get_backward_communication_data.\n";
-            return std::vector<int3double>();
-        }
+        return backward_communication_data[subdomain_id][neighbouring_subdomain_id];
+    }
+
+    int get_forward_communication_data_size(int subdomain_id, int neighbouring_subdomain_id) const {
+        return forward_communication_data[subdomain_id][neighbouring_subdomain_id].size();
+    }
+    int get_backward_communication_data_size(int subdomain_id, int neighbouring_subdomain_id) const {
+        return backward_communication_data[subdomain_id][neighbouring_subdomain_id].size();
     }
 
 };
